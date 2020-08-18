0ba474b5ff30eb4f2c7d4c214e68c5f9c4002795
yanchen01 <ychen01@bu.edu>
2020-08-18 13:11:41 -0400

    added animations for card and desc text

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -12,6 +12,7 @@ import sentithinkPic from '../../assets/images/sentithink.png';
 const Projects = [
 	{
 		title: 'Burger Builder',
+		link: 'https://github.com/yanchen01/burger-builder',
 		techStack: [ 'React', 'Firebase', 'HTML', 'CSS' ],
 		description: `Burger Builder is a web application that allows users to dynamically build their ideal burgers. 
 						The app allows users to checkout and submit burger orders.`,
@@ -19,6 +20,7 @@ const Projects = [
 	},
 	{
 		title: 'Sapflow Analytics',
+		link: 'https://sapflow.herokuapp.com/',
 		techStack: [ 'Node.js', 'Express', 'MongoDB', 'HTML', 'CSS' ],
 		description: `Sapflow Analytics is a web visualization platform that manage and visualize sensor data in real time. 
 						It serves as an internal tool for Hutyra Research Lab at Boston University`,
@@ -26,6 +28,7 @@ const Projects = [
 	},
 	{
 		title: 'Yelp Camp',
+		link: 'https://immense-peak-34617.herokuapp.com/',
 		techStack: [ 'Node.js', 'Express', 'MongoDB', 'HTML', 'CSS' ],
 		description: `Yelp Camp is a review forum web application for campgrounds. 
 						Similar to Yelp, users can post reviews, like/dislike, and comment on campground posts`,
@@ -33,12 +36,14 @@ const Projects = [
 	},
 	{
 		title: 'Terrier Exchange',
+		link: 'https://github.com/nicolekwon/TerrierExchange',
 		techStack: [ 'Android Studio', 'Java', 'Firebase' ],
 		description: `Terrier Exchange is an online marketplace Android app for Boston University students to buy, sell, and sell textbooks.`,
 		image: terrierExchangePic
 	},
 	{
 		title: 'Sentithink',
+		link: '#sentithink',
 		techStack: [ 'C#', 'Python', 'MySQL', 'JavaScript', 'HTML', 'CSS' ],
 		description: `Sentithink is a sentiment analysis web application that 
 						utilizes Natural Language Processing techniques to generate a sentiment heat map of a large group of people.`,
@@ -59,7 +64,9 @@ const Portfolio = () => (
 						<Card className={styles.Card}>
 							<Card.Img className={styles.Image} variant="top" src={project.image} />
 							<Card.Body>
-								<Card.Title>{project.title}</Card.Title>
+								<Card.Title>
+									<a href={project.link}>{project.title}</a>
+								</Card.Title>
 								<div className={styles.TechStackTags}>
 									{project.techStack.map((tech) => (
 										<div className={styles.TechStackTag}>
