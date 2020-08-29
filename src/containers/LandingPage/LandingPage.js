0f584d6c5870311b96b9ada5a72999782371ccb3
yanchen01 <ychen01@bu.edu>
2020-08-29 14:57:52 -0400

    added loading animation

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from '../../components/UI/Navigation/Navbar/Navbar';
 import Hero from '../../components/Hero/Hero';
 import Skills from '../../components/Skills/Skills';
@@ -6,13 +6,35 @@ import Portfolio from '../../components/Portfolio/Portfolio';
 import Footer from '../../components/Footer/Footer';
 
 const LandingPage = () => {
+	const [ loading, setLoading ] = useState(true);
+
+	const sleep = (milliseconds) => {
+		return new Promise((resolve) => setTimeout(resolve, milliseconds));
+	};
+
+	const animateLoading = async (milliseconds = 2000) => {
+		await sleep(milliseconds);
+		setLoading(false);
+	};
+
+	useEffect(() => {
+		animateLoading(2000);
+		console.log('Rendering');
+	});
+
 	return (
 		<div>
-			<NavBar />
-			<Hero />
-			<Skills />
-			<Portfolio />
-			<Footer />
+			{loading ? (
+				<div className="loader loader-curtain is-active" />
+			) : (
+				<React.Fragment>
+					<NavBar />
+					<Hero />
+					<Skills />
+					<Portfolio />
+					<Footer />
+				</React.Fragment>
+			)}
 		</div>
 	);
 };
